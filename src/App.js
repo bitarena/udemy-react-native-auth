86968453cf2b7838bdefa8b878b6eaab2cfe789c
David Muro <david.muro9@gmail.com>
2017-12-31 00:27:36 +0100

    auth flow finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+import config from './firebase-config';
 
 class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp(config);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+       return <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>;
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
         <View>
           <Header headerText="Authentication" />
-          <Text>An app</Text>
+          {this.renderContent()}
         </View>
     );
   }
